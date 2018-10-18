1d6c55e1dd1fbd51e2fd1cf8262a01c3bd9832a7
gladson <gladsonbrito@gmail.com>
2018-10-18 13:31:50 -0400

    inicio do estudo - AdonisJs

---
+++ app/Controllers/Http/AppController.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class AppController {
+  index() {
+    return "Ola Mundo!"
+  }
+}
+
+module.exports = AppController
---
+++ start/routes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('/register', 'AuthController.register')
+Route.post('/authenticate', 'AuthController.authenticate')
+
+Route.get('/app', "AppController.index").middleware(['auth'])
+
+Route.get('/', () => {
+  return { greeting: 'AQUI NAO TEM NADA!' }
+})
